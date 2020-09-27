88e0392ce151045d63793dd69b0efe1931fba6c0
Wassim Mansouri <wassim.mansouri@gmail.com>
2020-09-27 21:23:36 +0200

    Add custom fonts and avatar

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,8 +10,35 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.greenAccent,
-        body: Container(),
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            children: [
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/me.jpg'),
+              ),
+              Text(
+                'Wassim Mansouri',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              Text(
+                'mobile interaction designer/developer'.toUpperCase(),
+                style: TextStyle(
+                  fontFamily: 'SourceCodePro',
+                  fontSize: 15.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.teal[100],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
